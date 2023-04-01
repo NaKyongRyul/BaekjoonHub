70078091776386dc3877c1f7c38206e8cca603b0
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-04-01 12:52:22 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1556 ms, Memory: 123916 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,29 +1,26 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] A, B;
+    static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    
+
+    static int N;
+    static int[] A;
+
     static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        A = new int[N+1];
-        for(int i=1; i<=N; i++) {
-            A[i] = sc.nextInt();
-        }
-        M = sc.nextInt();
-        B = new int[M+1];
-        for(int i=1; i<=M; i++) {
-            B[i] = sc.nextInt();
+        N = scan.nextInt();
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = scan.nextInt();
         }
     }
 
     static int lower_bound(int[] A, int L, int R, int X) {
-        int ans = R+1;
-        while(L<=R) {
-            int mid = (L+R)/2;
-            if(A[mid] >= X) {
+        int ans = R + 1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (A[mid] >= X) {
                 ans = mid;
                 R = mid - 1;
             } else {
@@ -34,10 +31,10 @@ public class Main {
     }
 
     static int upper_bound(int[] A, int L, int R, int X) {
-        int ans = R+1;
-        while(L<=R) {
-            int mid = (L+R)/2;
-            if(A[mid] > X) {
+        int ans = R + 1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (A[mid] > X) {
                 ans = mid;
                 R = mid - 1;
             } else {
@@ -48,17 +45,66 @@ public class Main {
     }
 
     static void pro() {
-        Arrays.sort(A, 1, N+1);
-        for(int i=1; i<=M; i++) {
-            int lower = lower_bound(A, 1, N, B[i]);
-            int upper = upper_bound(A, 1, N, B[i]);
+        int M = scan.nextInt();
+        Arrays.sort(A, 1, N + 1);
+        for (int i = 1; i <= M; i++) {
+            int X = scan.nextInt();
+            int upper = upper_bound(A, 1, N, X);
+            int lower = lower_bound(A, 1, N, X);
             sb.append(upper - lower).append(' ');
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
     public static void main(String[] args) {
         input();
         pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
