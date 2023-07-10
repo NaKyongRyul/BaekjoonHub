3f5c618bca4d1dafd3a140e9f084e557cffa876c
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-07-10 15:44:40 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 640 ms, Memory: 59780 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());//수의 개수 
+		int count = Integer.parseInt(st.nextToken());// 반복할 횟수 
+		
+		int[] arr = new int[N+1];
+		int[] sum = new int[N+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum[i] = sum[i-1] + arr[i]; 
+		}
+		
+		for(int i=0; i<count; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			int result = sum[to] - sum[from-1];
+			sb.append(result + "\n");
+		}
+		System.out.println(sb);
+	}
+
+}
