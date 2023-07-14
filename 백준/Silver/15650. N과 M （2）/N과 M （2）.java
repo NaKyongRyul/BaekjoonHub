55403c0c2567d52ba54e045cf29584d7d51566f0
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-07-14 12:42:30 +0900

    [Silver III] Title: N과 M (2), Time: 140 ms, Memory: 15808 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,35 +1,38 @@
-import java.util.Scanner;
-//완전 탐색(4)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] selected;
+	
+	public static void rec_func(int k) {
+		if(k == M+1) {
+			for(int i=1; i<=M; i++) {
+				sb.append(selected[i] + " ");
+			}
+			sb.append("\n");
+		} else {
+			for(int cand=selected[k-1]+1; cand<=N; cand++) {
+				selected[k] = cand;
+				rec_func(k+1);
+			}
+		}
+		
+	}
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        arr = new int[M+1];
-    }
-
-    static void rec_func(int k) {
-        if(k == M+1) {
-            for(int i=1; i<=M; i++) {
-                sb.append(arr[i]).append(' ');
-            }
-            sb.append('\n');
-        }
-        else {
-            for(int cand=arr[k-1]+1; cand<=N; cand++) {
-                arr[k] = cand;
-                rec_func(k+1);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        input();
-        rec_func(1);
-        System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		selected = new int[M+1];
+		
+		rec_func(1);
+		System.out.println(sb);
+	}
 }
