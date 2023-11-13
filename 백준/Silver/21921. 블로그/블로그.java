2a4cb2c1cfcaf70dc273cf2bc3c07cca4c9c3502
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-11-13 14:05:53 +0900

    [Silver III] Title: 블로그, Time: 388 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,45 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, X;
+    static int[] arr;
+    static int max, cnt;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[N+1];
+        N = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        arr = new int[N+1];
+        max = 0;
 
         st = new StringTokenizer(br.readLine());
-//        arr[0] = Integer.parseInt(st.nextToken());
-        for (int i = 1; i <= N; i++) {
-            arr[i] += arr[i - 1] + Integer.parseInt(st.nextToken());
+        for(int i=1; i<N+1; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
         }
 
-        if (arr[N] == 0) {
+        if(arr[N] == 0) {
             sb.append("SAD");
             System.out.println(sb);
             return;
         }
 
-        int max = 0;
-        int result = 0;
-        int count = 0;
-
-        for (int i = X; i <= N; i++) {
-            result = arr[i] - arr[i-X];
-            if (result > max) {
-                max = result;
-                count = 1;
-            } else if (result == max) {
-                count++;
+        for(int i=0; i<N-X+1; i++) {
+            if(arr[X+i] - arr[i] > max) {
+                max = arr[X+i] - arr[i];
+                cnt = 1;
+            } else if(arr[X+i] - arr[i] == max) {
+                cnt++;
             }
         }
-        sb.append(max + "\n" + count);
+        sb.append(max + "\n" + cnt);
         System.out.println(sb);
     }
 }
