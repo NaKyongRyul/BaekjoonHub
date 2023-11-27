f968a475da9cfaed46e46404e7db3444361eab94
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-11-27 23:15:54 +0900

    [Gold V] Title: 문자열 게임 2, Time: 612 ms, Memory: 23064 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,44 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
+    static int T;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        int t = Integer.parseInt(br.readLine());
-        for(int i = 0; i < t; i++) {
-            String str = br.readLine();
+        T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++) {
+            String w = br.readLine();
             int k = Integer.parseInt(br.readLine());
 
             if(k == 1) {
-                System.out.println("1 1");
+                sb.append("1 1" + "\n");
                 continue;
             }
-
-            int[] alpha = new int[26];
-            for(int j = 0; j < str.length(); j++) {
-                alpha[str.charAt(j) - 'a']++;
+            int[] alphabet = new int[26];
+            for(int j=0; j<w.length(); j++) {
+                alphabet[w.charAt(j)-'a']++;
             }
-
-            int min = Integer.MAX_VALUE;
             int max = -1;
-            for(int j = 0; j < str.length(); j++) {
-                if(alpha[str.charAt(j) - 'a'] < k) continue;
-
-                int count = 1;
-                for(int l = j + 1; l < str.length(); l++) {
-                    if(str.charAt(j) == str.charAt(l)) count++;
-                    if(count == k) {
-                        min = Math.min(min, l - j + 1);
-                        max = Math.max(max, l - j + 1);
+            int min = Integer.MAX_VALUE;
+            for(int j=0; j<w.length(); j++) {
+                if(alphabet[w.charAt(j)-'a'] < k) continue;
+                int cnt = 1;
+                for(int n=j+1; n<w.length(); n++) {
+                    if(w.charAt(j) == w.charAt(n)) cnt++;
+                    if(cnt == k) {
+                        min = Math.min(min, n-j+1);
+                        max = Math.max(max, n-j+1);
                         break;
                     }
                 }
             }
-            if(min == Integer.MAX_VALUE || max == -1) System.out.println("-1");
-            else System.out.println(min + " " + max);
+            if(min == Integer.MAX_VALUE || max == -1) sb.append("-1" + "\n");
+            else sb.append(min + " " + max + "\n");
         }
+        System.out.println(sb);
     }
 }
