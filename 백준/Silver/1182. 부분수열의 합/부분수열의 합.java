32d0f6e33644c7866736a8bb3f6d7ae3dfe5da6e
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-12-02 10:08:53 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,42 +1,38 @@
-import java.util.Scanner;
-//완전 탐색
-//크기가 양수인 부분수열 -> 원소가 1개 이상인 부분수열
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    static StringBuilder sb = new StringBuilder();
     static int N, S;
     static int[] arr;
-    static int count;
+    static int cnt;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        S = sc.nextInt();
-        arr = new int[N+1];
-        for(int i=1; i<=N; i++) {
-            arr[i] = sc.nextInt();
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        dfs(0,0);
+        sb.append(S == 0 ? cnt-1 : cnt);
+        System.out.println(sb);
     }
 
-
-
-    static void rec_func(int k, int value) {
-        if(k == N+1) {
-            if(value == S) {
-                count++;
+    public static void dfs(int depth, int sum) {
+        if(depth == N) {
+            if(sum == S) {
+                cnt++;
             }
+            return;
         }
-        else {
-            rec_func(k+1, value+arr[k]);
-            rec_func(k+1, value);
-        }
-    }
-
-    public static void main(String[] args) {
-        input();
-        rec_func(1, 0);
-        if(S == 0) {
-            count--;
-        }
-        System.out.println(count);
+        dfs(depth+1, sum+arr[depth]);
+        dfs(depth+1, sum);
     }
 }
