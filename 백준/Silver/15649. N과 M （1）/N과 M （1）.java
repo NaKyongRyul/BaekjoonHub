8948ee1ea9238e6b1e404f0317058d069f9d972e
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-12-23 16:15:22 +0900

    [Silver III] Title: N과 M (1), Time: 324 ms, Memory: 25008 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,45 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.Scanner;
+//완전 탐색(2)
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static int N;
-	static int M;
-	static int[] selected, used;
-	
-	public static void rec_func(int k) {
-		if(k == M+1) {
-			for(int i=1; i<=M; i++) {
-				sb.append(selected[i] + " ");
-			}
-			sb.append("\n");
-		} else {
-			for(int cand=1; cand<=N; cand++) {
-				if(used[cand] == 1) {
-					continue;
-				}
-				selected[k] = cand;
-				used[cand] = 1;
-				//k+1 ~ M을 모두 탐색 
-				rec_func(k+1);
-				used[cand] = 0;
-			}
-		}
-	}
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr, used;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		selected = new int[M+1];
-		used = new int[N+1];
-		
-		rec_func(1);
-		System.out.println(sb);
-	}
+    static void input() {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[M];
+        used = new int[N+1];
+    }
 
+    static void rec_func(int k) {
+        if(k == M) {
+            for(int i=0; i<M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+        }
+        else {
+            for(int cand=1; cand<=N; cand++) {
+                if(used[cand] == 1) {
+                    continue;
+                }
+                arr[k] = cand;
+                used[cand] = 1;
+                rec_func(k+1);
+                used[cand] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        rec_func(0);
+        System.out.println(sb.toString());
+    }
 }
