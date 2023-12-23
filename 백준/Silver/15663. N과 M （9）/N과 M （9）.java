69e34f9b5743aed1e64b6b2627dd133da3f0cfac
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-12-23 18:14:47 +0900

    [Silver II] Title: N과 M (9), Time: 272 ms, Memory: 28640 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,51 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
-    static int[] arr_N;
-    static int[] arr_M;
+    static int[] arr;
+    static int[] selected;
     static boolean[] isVisit;
-    static StringBuilder sb;
+
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr_N = new int[N];
-        arr_M = new int[M];
+        arr = new int[N];
+        selected = new int[M];
         isVisit = new boolean[N];
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
-            arr_N[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr_N);
-        dfs(0);
+
+        Arrays.sort(arr);
+        rec_func(0);
         System.out.println(sb);
     }
 
-    static void dfs(int depth) {
-        if(depth == M) {
-            for(int m : arr_M) {
-                sb.append(m + " ");
+    static void rec_func(int x) {
+        if(x == M) {
+            for(int i=0; i<M; i++) {
+                sb.append(selected[i] + " ");
             }
             sb.append("\n");
-            return;
-        }
-
-        int beforeNum = 0;
-        for(int i=0; i<N; i++) {
-            if(isVisit[i]) continue;
-            if(beforeNum != arr_N[i]) {
+        } else {
+            int prev = 0;
+            for(int i=0; i<N; i++) {
+                if(isVisit[i] || prev == arr[i]) continue;
                 isVisit[i] = true;
-                arr_M[depth] = arr_N[i];
-                beforeNum = arr_N[i];
-                dfs(depth+1);
+                selected[x] = arr[i];
+                prev = arr[i];
+                rec_func(x+1);
                 isVisit[i] = false;
             }
         }
