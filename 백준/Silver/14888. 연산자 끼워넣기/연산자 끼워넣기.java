6ee25be8e65ddf749c8575b1c324a506e70e628b
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-12-23 22:31:18 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 168 ms, Memory: 16656 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,73 +1,73 @@
-import java.util.Scanner;
-//완전 탐색
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int N, max, min;
-    static int[] nums, operators, orders;
+    static int N;
+    static int[] arr;
+    static int[] operator;
+    static int max, min;
+    static int[] selected;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        //전체 숫자가 저장된 배열
-        nums = new int[N+1];
-        operators = new int[5];
-        //연산자가 저장될 위치
-        orders = new int[N];
-        for(int i=1; i<=N; i++) {
-            nums[i] = sc.nextInt();
+        N = Integer.parseInt(br.readLine());
+        max = Integer.MIN_VALUE;
+        min = Integer.MAX_VALUE;
+        arr = new int[N];
+        operator = new int[4];
+        selected = new int[N-1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        for(int i=1; i<=4; i++) {
-            operators[i] = sc.nextInt();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<4; i++) {
+            operator[i] = Integer.parseInt(st.nextToken());
         }
-        //무조건 더 큰값이 되기 위해 가장 작은값으로 설정
-        max=Integer.MIN_VALUE;
-        //무조건 더 작은값이 되기 위해 가장 큰값으로 설정
-        min=Integer.MAX_VALUE;
+
+        rec_func(0);
+        sb.append(max + "\n" + min);
+        System.out.println(sb);
     }
 
-    static int calculator() {
-        int value = nums[1];
-        for(int i=1; i<=N-1; i++) {
-            if(orders[i] == 1) {
-                value += nums[i+1];
-            }
-            if(orders[i] == 2) {
-                value -= nums[i+1];
-            }
-            if(orders[i] == 3) {
-                value *= nums[i+1];
-            }
-            if(orders[i] == 4) {
-                value /= nums[i+1];
-            }
-        }
-        return value;
-    }
-
-
-    static void rec_func(int k) {
-        if(k == N) {
-            int value = calculator();
-            max = Math.max(max, value);
-            min = Math.min(min, value);
-        }
-        else {
-            for(int cand=1; cand<=4; cand++) {
-                if(operators[cand] >= 1) {
-                    operators[cand]--;
-                    orders[k] = cand;
-                    rec_func(k+1);
-                    operators[cand]++;
-                    orders[k] = 0;
+    //dfs? -> ㅇㅇ
+    static void rec_func(int x) {
+        if(x == N-1) {
+            int val = calculator();
+            max = Math.max(max, val);
+            min = Math.min(min, val);
+        } else {
+            for(int i=0; i<4; i++) {
+                if(operator[i] > 0) {
+                    selected[x] = i;
+                    operator[i]--;
+                    rec_func(x+1);
+                    operator[i]++;
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
-        input();
-        rec_func(1);
-        sb.append(max).append('\n').append(min);
-        System.out.println(sb.toString());
+    static int calculator() {
+        int val = arr[0];
+        for(int i=0; i<N-1; i++) {
+            if(selected[i] == 0) {
+                val = val + arr[i+1];
+            } else if(selected[i] == 1) {
+                val = val - arr[i+1];
+            } else if(selected[i] == 2) {
+                val = val * arr[i+1];
+            } else if(selected[i] == 3) {
+                val = val / arr[i+1];
+            }
+        }
+        return val;
     }
 }
