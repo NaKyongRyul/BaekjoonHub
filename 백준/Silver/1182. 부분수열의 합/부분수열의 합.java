9460400dcd35197a44155c54962128a540e660d6
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2023-12-24 12:48:53 +0900

    [Silver II] Title: 부분수열의 합, Time: 128 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,38 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, S;
-    static int[] arr;
-    static int cnt;
+    static int N, M, ans;
+    static int[] nums;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        arr = new int[N];
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-        dfs(0,0);
-        sb.append(S == 0 ? cnt-1 : cnt);
+
+        if(M == 0) ans--;
+        rec_func(0, 0);
+        sb.append(ans);
         System.out.println(sb);
     }
 
-    public static void dfs(int depth, int sum) {
-        if(depth == N) {
-            if(sum == S) {
-                cnt++;
-            }
-            return;
+    static void rec_func(int x, int sum) {
+        if(x == N) {
+            if(M == sum) ans++;
+        } else {
+            rec_func(x+1, sum+nums[x]);
+            rec_func(x+1, sum);
         }
-        dfs(depth+1, sum+arr[depth]);
-        dfs(depth+1, sum);
     }
 }
