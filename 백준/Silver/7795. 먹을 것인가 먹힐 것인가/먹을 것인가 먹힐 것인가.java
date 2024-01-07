929c8b5d4ecac9dc54e7a862dd31345cc9ac6ea8
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2024-01-07 13:22:20 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 460 ms, Memory: 38560 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,108 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
+    static int T, N, M;
+    static int[] arr_A, arr_B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    static int N, M;
-    static int[] A, B;
+        T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
 
-    static void input() {
-        N = scan.nextInt();
-        M = scan.nextInt();
-        A = new int[N + 1];
-        B = new int[M + 1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = scan.nextInt();
-        }
-        for (int i = 1; i <= M; i++) {
-            B[i] = scan.nextInt();
+            arr_A = new int[N];
+            arr_B = new int[M+1];
+
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++) {
+                arr_A[j] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=M; j++) {
+                arr_B[j] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr_B);
+
+            int ans = 0;
+            for(int n=0; n<N; n++) {
+                ans += binary_search(arr_B, 1, M, arr_A[n]);
+            }
+            System.out.println(ans);
         }
     }
-
-    static int lower_bound(int[] A, int L, int R, int X) {
-        // A[L...R] 에서 X 미만의 수 중 제일 오른쪽 인덱스를 return 하는 함수
-        // 그런 게 없다면 L - 1 을 return 한다
-
-        int res = L - 1;  // 만약 A[L...R] 중 X 이하의 수가 없다면 L - 1 을 return 한다.
-        while (L <= R) {
+    static int binary_search(int[] B, int L, int R, int X) {
+        int result = 0;
+        while(L <= R) {
             int mid = (L + R) / 2;
-            if (A[mid] < X) {
-                res = mid;
+            if(B[mid] < X) {
+                result = mid;
                 L = mid + 1;
-            } else {
+            } else if(B[mid] >= X) {
                 R = mid - 1;
             }
         }
-        return res;
+        return result;
     }
 
-    static void pro() {
-        // B 배열에 대해 이분탐색을 할 예정이니까, 정렬을 해주자!
-        Arrays.sort(B, 1, M + 1);
-        
-        int ans = 0;
-        for (int i = 1; i <= N; i++) {
-            // A[i] 를 선택했을 때, B 에서는 A[i]보다 작은 게 몇 개나 있는 지 count하기
-            ans += lower_bound(B, 1, M, A[i]);
-        }
-        System.out.println(ans);
-    }
-
-    public static void main(String[] args) {
-        int TT;
-        TT = scan.nextInt();
-        for (int tt = 1; tt <= TT; tt++) {
-            input();
-            pro();
-        }
-    }
-
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
 }
