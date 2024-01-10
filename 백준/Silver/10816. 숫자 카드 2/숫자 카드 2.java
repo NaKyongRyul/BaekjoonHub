fcfa78254a893e8c1297dc20c256a409e26aba29
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2024-01-10 21:47:46 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1148 ms, Memory: 183536 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,110 +1,33 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static HashMap<Integer, Integer> map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-    static int N;
-    static int[] A;
+        N = Integer.parseInt(br.readLine());
+        map = new HashMap<>();
 
-    static void input() {
-        N = scan.nextInt();
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = scan.nextInt();
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0)+1);
         }
-    }
 
-    static int lower_bound(int[] A, int L, int R, int X) {
-        int ans = R + 1;
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (A[mid] >= X) {
-                ans = mid;
-                R = mid - 1;
-            } else {
-                L = mid + 1;
-            }
-        }
-        return ans;
-    }
+        M = Integer.parseInt(br.readLine());
 
-    static int upper_bound(int[] A, int L, int R, int X) {
-        int ans = R + 1;
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (A[mid] > X) {
-                ans = mid;
-                R = mid - 1;
-            } else {
-                L = mid + 1;
-            }
-        }
-        return ans;
-    }
-
-    static void pro() {
-        int M = scan.nextInt();
-        Arrays.sort(A, 1, N + 1);
-        for (int i = 1; i <= M; i++) {
-            int X = scan.nextInt();
-            int upper = upper_bound(A, 1, N, X);
-            int lower = lower_bound(A, 1, N, X);
-            sb.append(upper - lower).append(' ');
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(num, 0) + " ");
         }
         System.out.println(sb);
     }
 
-    public static void main(String[] args) {
-        input();
-        pro();
-    }
-
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
 }
