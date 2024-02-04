4680143024b5241432ab06b7974465ab9b7fe508
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2024-02-05 08:20:26 +0900

    [Silver V] Title: 배열 합치기, Time: 1708 ms, Memory: 330480 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,51 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class Main {
+    static int N, M;
+    static int[] arr_A, arr_B, arr_all;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int[] arr1 = new int[N];
-        int[] arr2 = new int[M];
+        arr_A = new int[N];
+        arr_B = new int[M];
+        arr_all = new int[N+M];
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken());
+            arr_A[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<M; i++) {
-            arr2[i] = Integer.parseInt(st.nextToken());
+            arr_B[i] = Integer.parseInt(st.nextToken());
         }
 
-        int arr1_i = 0;
-        int arr2_i = 0;
-
-        while(arr1_i < N && arr2_i < M) {
-            if(arr1[arr1_i] >= arr2[arr2_i]) {
-                sb.append(arr2[arr2_i++] + " ");
-            } else {
-                sb.append(arr1[arr1_i++] + " ");
-            }
+        for(int i=0; i<N; i++) {
+            arr_all[i] = arr_A[i];
         }
 
-        if(arr1_i == N) {
-            for(int i=arr2_i; i<M; i++) {
-                sb.append(arr2[i] + " ");
-            }
+        for(int i=0; i<M; i++) {
+            arr_all[i+N] = arr_B[i];
         }
 
-        if(arr2_i == M) {
-            for(int i=arr1_i; i<N; i++) {
-                sb.append(arr1[i] + " ");
-            }
+        Arrays.sort(arr_all);
+
+        for(int i : arr_all) {
+            sb.append(i + " ");
         }
         System.out.println(sb);
     }
