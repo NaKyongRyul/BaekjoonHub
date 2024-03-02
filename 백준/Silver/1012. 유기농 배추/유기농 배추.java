eb8001fc6b11ff117cf93aa63dd07a79e6d9ac75
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2024-03-02 21:18:16 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,79 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node {
-    int x, y;
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main {
-    static int T, M, N, K, cnt;
+    static int T, M, N, K;
     static int[][] arr;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[][] dist = {{-1,0}, {1,0}, {0,-1}, {0,1}};
     static boolean[][] isVisit;
-
+    static int group_cnt;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb;
+        StringBuilder sb = new StringBuilder();
 
         T = Integer.parseInt(br.readLine());
 
-        for(int t=0; t<T; t++) {
+        for(int i=0; i<T; i++) {
             st = new StringTokenizer(br.readLine());
-            sb = new StringBuilder();
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
+
             arr = new int[M][N];
             isVisit = new boolean[M][N];
-            cnt = 0;
+            group_cnt = 0;
 
-            for(int k=0; k<K; k++) {
+            for(int j=0; j<K; j++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-
                 arr[x][y] = 1;
             }
+            search();
+            sb.append(group_cnt).append("\n");
+        }
+        System.out.println(sb);
+    }
 
-            for(int i=0; i<M; i++) {
-                for(int j=0; j<N; j++) {
-                    if(arr[i][j] == 1 && !isVisit[i][j]) bfs(i,j);
-                }
-            }
-            sb.append(cnt);
-            System.out.println(sb);
+    static void dfs(int x, int y) {
+        isVisit[x][y] = true;
+        for(int i=0; i<4; i++) {
+            int nx = x + dist[i][0];
+            int ny = y + dist[i][1];
+
+            if(nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+            if(isVisit[nx][ny] || arr[nx][ny] == 0) continue;
+            dfs(nx, ny);
         }
     }
 
-    static void bfs(int a, int b) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(a, b));
-        isVisit[a][b] = true;
-        cnt++;
-
-        while(!queue.isEmpty()) {
-            Node node = queue.poll();
-
-            for(int i=0; i<4; i++) {
-                int x = node.x + dx[i];
-                int y = node.y + dy[i];
-
-                if(x<0 || y<0 || x>=M || y>=N) continue;
-                if(arr[x][y] == 1 && !isVisit[x][y]) {
-                    queue.add(new Node(x,y));
-                    isVisit[x][y] = true;
-                }
+    static void search() {
+        for(int i=0; i<M; i++) {
+            for(int j=0; j<N; j++) {
+                if(isVisit[i][j] || arr[i][j] == 0) continue;
+                group_cnt++;
+                dfs(i, j);
             }
         }
     }
+
+
 }
