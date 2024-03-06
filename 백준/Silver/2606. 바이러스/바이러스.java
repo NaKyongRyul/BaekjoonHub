cff1fd80ec10baa165d558d52e7d4b3246a6d755
Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>
2024-03-06 23:42:30 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,51 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int computer_cnt, line, ans;
-    static int[][] arr;
-
+    static int N, K, ans;
+    static ArrayList<Integer>[] arr;
+    static boolean[] isVisit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        computer_cnt = Integer.parseInt(br.readLine());
-        line = Integer.parseInt(br.readLine());
-        arr = new int[computer_cnt+1][computer_cnt+1];
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+        isVisit = new boolean[N+1];
+        arr = new ArrayList[N+1];
 
-        for(int i=0; i<line; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            arr[x][y] = 1;
-            arr[y][x] = 1;
+        for(int i=1; i<=N; i++) {
+            arr[i] = new ArrayList<>();
         }
-        sb.append(bfs());
+
+        for(int i=0; i<K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a].add(b);
+            arr[b].add(a);
+        }
+        dfs(1);
+        sb.append(ans);
         System.out.println(sb);
     }
 
-    public static int bfs() {
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] isVisit = new boolean[computer_cnt+1];
-        queue.offer(1);
-        isVisit[1] = true;
-
-        while(!queue.isEmpty()) {
-            int node = queue.poll();
-
-            for(int i=1; i<computer_cnt+1; i++) {
-                if(arr[node][i] == 1 && isVisit[i] == false) {
-                    queue.offer(i);
-                    isVisit[i] = true;
-                    ans++;
-                }
-            }
+    static void dfs(int x) {
+        isVisit[x] = true;
+        for(int i : arr[x]) {
+            if(isVisit[i]) continue;
+            ans++;
+            dfs(i);
         }
-        return ans;
     }
 }
