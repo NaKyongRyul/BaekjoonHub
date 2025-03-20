67037f9f4e9030783ea21f9bb83b0e1065a34ab5
NaKyongRyul <67616146+NaKyongRyul@users.noreply.github.com>
2025-03-20 23:32:20 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,26 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[26];
-		
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = -1;
-		}
-		
-		String s = sc.next();
-		
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<s.length(); j++) {
-				if(arr[s.charAt(j) - 'a'] == -1) {
-					arr[s.charAt(j) - 'a'] = j;
-				}
-			}
-			System.out.print(arr[i] + " ");
-		}
-		sc.close();
-	}
+        String word = br.readLine();
+        int[] alphabet = new int[26];
 
+        for(int i=0; i<26; i++) {
+            alphabet[i] = -1;
+        }
+
+        for(int i=0; i<word.length(); i++) {
+            char c = word.charAt(i);
+            if(alphabet[c-'a'] == -1) {
+                alphabet[c-'a'] = i;
+            }
+        }
+        for(int count : alphabet) {
+            sb.append(count).append(" ");
+        }
+        System.out.println(sb);
+    }
 }
