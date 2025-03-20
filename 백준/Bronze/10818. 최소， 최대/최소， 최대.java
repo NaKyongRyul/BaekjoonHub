c498557dc5958807a1579656c577f532acd650d4
NaKyongRyul <67616146+NaKyongRyul@users.noreply.github.com>
2025-03-20 22:12:54 +0900

    [Bronze III] Title: 최소, 최대, Time: 1196 ms, Memory: 103748 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,17 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int[] arr = new int[a];
-		
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		sc.close();
-		Arrays.sort(arr);
-		System.out.println(arr[0] + " " + arr[a-1]);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        sb.append(arr[0]).append(" ").append(arr[N-1]);
+        System.out.println(sb.toString());
+    }
 }
