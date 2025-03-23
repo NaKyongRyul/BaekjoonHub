80c8d972f1c8a1c57c1f4219739ab69941d96458
NaKyongRyul <67616146+NaKyongRyul@users.noreply.github.com>
2025-03-23 21:27:09 +0900

    [Silver III] Title: 프린터 큐, Time: 132 ms, Memory: 15044 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,54 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int testCase, n, m, count;
+    static class Document {
+        int index;
+        int priority;
+
+        public Document(int index, int priority) {
+            this.index = index;
+            this.priority = priority;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        testCase = Integer.parseInt(br.readLine());
+        int testcase = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<testCase; i++) {
+        for(int i=0; i<testcase; i++) {
             st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            st = new StringTokenizer(br.readLine());
-            LinkedList<int[]> queue = new LinkedList<>();
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
-            for(int j=0; j<n; j++) {
-                queue.offer(new int[] {
-                        j, Integer.parseInt(st.nextToken())
-                });
+            Queue<Document> queue = new LinkedList<>();
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++) {
+                int priority = Integer.parseInt(st.nextToken());
+                queue.offer(new Document(j, priority));
+                priorityQueue.offer(priority);
             }
 
-            count = 0;
+            int printOrder = 0;
 
             while(!queue.isEmpty()) {
-                int[] firstNum = queue.poll();
-                boolean isMax = true;
+                Document currentDocument = queue.poll();
+                if(currentDocument.priority == priorityQueue.peek()) {
+                    printOrder++;
+                    priorityQueue.poll();
 
-                for(int j=0; j<queue.size(); j++) {
-                    if(firstNum[1] < queue.get(j)[1]) {
-                        queue.offer(firstNum);
-                        for(int z=0; z<j; z++) {
-                            queue.offer(queue.poll());
-                        }
-                        isMax = false;
+                    if(currentDocument.index == M) {
+                        sb.append(printOrder).append("\n");
                         break;
                     }
+                } else {
+                  queue.offer(currentDocument);
                 }
-                if(isMax == false) continue;
-
-                count++;
-                if(firstNum[0] == m) break;
             }
-            sb.append(count +"\n");
         }
         System.out.println(sb);
     }
