05cb8e22b546f22f6bc0c933965e18a17193b0b0
NaKyongRyul <67616146+NaKyongRyul@users.noreply.github.com>
2025-04-02 00:02:08 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -5,64 +5,59 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node {
-    int x, y;
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main {
     static int N, M;
-    static int[][] arr;
+    static boolean[][] visit;
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};;
     static int[][] dist;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static String[] s;
 
-    public static void main(String[] args) throws IOException{
+    static void bfs(int x, int y) {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                dist[i][j] = -1;
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(x);
+        queue.add(y);
+        dist[x][y] = 1;
+        visit[x][y] = true;
+
+        while(!queue.isEmpty()) {
+            x = queue.poll();
+            y = queue.poll();
+            for(int k=0; k<4; k++) {
+                int nx = x + dir[k][0];
+                int ny = y + dir[k][1];
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if(visit[nx][ny]) continue;
+                if(s[nx].charAt(ny) == '0') continue;
+                queue.add(nx);
+                queue.add(ny);
+                visit[nx][ny] = true;
+                dist[nx][ny] = dist[x][y] + 1;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());;
         StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N+1][M+1];
-        dist = new int[N+1][M+1];
 
-        for(int i=1; i<=N; i++) {
-            String s = br.readLine();
-            for(int j=1; j<=M; j++) {
-                arr[i][j] = (int)(s.charAt(j-1)-'0');
-            }
+        s = new String[N];
+        visit = new boolean[N][M];
+        dist = new int[N][M];
+
+        for(int i=0; i<N; i++) {
+            s[i] = br.readLine();
         }
 
-        bfs(1,1);
-        sb.append(dist[N][M]);
+        bfs(0,0);
+        sb.append(dist[N-1][M-1]);
         System.out.println(sb);
-    }
-
-    static void bfs(int a, int b) {
-        Queue<Node> queue = new LinkedList<>();
-        boolean[][] isVisit = new boolean[N+1][M+1];
-        queue.add(new Node(a, b));
-        isVisit[a][b] = true;
-        dist[a][b] = 1;
-
-        while(!queue.isEmpty()) {
-            Node node = queue.poll();
-
-            for(int i=0; i<4; i++) {
-                int x = node.x + dx[i];
-                int y = node.y + dy[i];
-
-                if(x<1 || y<1 || x>=N+1 || y>=M+1) continue;
-                if(arr[x][y] == 1 && !isVisit[x][y]) {
-                    queue.add(new Node(x,y));
-                    isVisit[x][y] = true;
-                    dist[x][y] = dist[node.x][node.y] + 1;
-                }
-            }
-        }
     }
 }
