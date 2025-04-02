d4768ccf2935bbe1e5e0b7fcc4889ba20dccc1a5
NaKyongRyul <67616146+NaKyongRyul@users.noreply.github.com>
2025-04-02 23:20:50 +0900

    [Silver II] Title: DFS와 BFS, Time: 204 ms, Memory: 20260 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -5,67 +5,70 @@ import java.util.*;
 
 public class Main {
     static int N, M, V;
-    static ArrayList<Integer>[] arr;
-    static boolean[] isVisit;
-    static StringBuilder sb = new StringBuilder();
+    static ArrayList<Integer>[] adj;
+    static boolean[] visit;
+    static StringBuilder sb;
+
+    static void dfs(int x) {
+        visit[x] = true;
+        sb.append(x).append(" ");
+
+        for(int y : adj[x]) {
+            if(visit[y]) continue;
+            dfs(y);
+        }
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+
+        visit = new boolean[N+1];
+        visit[start] = true;
+
+        while(!queue.isEmpty()) {
+            int x = queue.poll();
+            sb.append(x).append(" ");
+
+            for(int y : adj[x]) {
+                if(visit[y]) continue;
+                queue.add(y);
+                visit[y] = true;
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         V = Integer.parseInt(st.nextToken());
-        arr = new ArrayList[N+1];
+        adj = new ArrayList[N+1];
+        visit = new boolean[N+1];
+
         for(int i=1; i<=N; i++) {
-            arr[i] = new ArrayList<>();
+            adj[i] = new ArrayList<>();
         }
 
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            arr[x].add(y);
-            arr[y].add(x);
+
+            adj[x].add(y);
+            adj[y].add(x);
         }
 
         for(int i=1; i<=N; i++) {
-            Collections.sort(arr[i]);
+            Collections.sort(adj[i]);
         }
 
-        isVisit = new boolean[N+1];
         dfs(V);
         sb.append("\n");
-
         bfs(V);
         System.out.println(sb);
     }
-
-    static void dfs(int x) {
-        isVisit[x] = true;
-        sb.append(x + " ");
-        for(int y : arr[x]) {
-            if(isVisit[y]) continue;
-            dfs(y);
-        }
-    }
-
-    static void bfs(int x) {
-        Queue<Integer> queue = new LinkedList<>();
-        isVisit = new boolean[N+1];
-
-        queue.add(x);
-        isVisit[x] = true;
-
-        while(!queue.isEmpty()) {
-            x = queue.poll();
-            sb.append(x + " ");
-            for(int y : arr[x]) {
-                if(isVisit[y]) continue;
-                queue.add(y);
-                isVisit[y] = true;
-            }
-        }
-    }
-
 }
