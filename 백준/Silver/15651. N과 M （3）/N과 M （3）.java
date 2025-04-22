d672b555c5d901b3c62736d5e267abf674998675
NaKyongRyul <67616146+NaKyongRyul@users.noreply.github.com>
2025-04-22 21:22:01 +0900

    [Silver III] Title: N과 M (3), Time: 388 ms, Memory: 67876 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,35 +1,37 @@
-import java.util.Scanner;
-//완전 탐색(1)
-public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static int[] arr;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        arr = new int[M];
-    }
+public class Main {
+    static int N, M;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
 
     static void rec_func(int k) {
-        if(k == M) {
-            for(int i=0; i<M; i++) {
-                sb.append(arr[i]).append(' ');
+        if(k == M+1) {
+            for(int i=1; i<=M; i++) {
+                sb.append(selected[i]).append(" ");
             }
-            sb.append('\n');
-        }
-        else {
-            for(int cand=1; cand<=N; cand++) {
-                arr[k] = cand;
+            sb.append("\n");
+        } else {
+            for(int i=1; i<=N; i++) {
+                selected[k] = i;
                 rec_func(k+1);
+                selected[k] = 0;
             }
         }
     }
 
-    public static void main(String[] args) {
-        input();
-        rec_func(0);
-        System.out.println(sb.toString());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M+1];
+
+        rec_func(1);
+        System.out.println(sb);
     }
 }
