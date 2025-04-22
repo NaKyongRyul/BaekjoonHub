4a0c9636cd4a65836c72c8fb2c17af256553d585
NaKyongRyul <67616146+NaKyongRyul@users.noreply.github.com>
2025-04-22 21:38:37 +0900

    [Silver III] Title: N과 M (1), Time: 256 ms, Memory: 23224 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,41 +1,46 @@
-import java.util.Scanner;
-//완전 탐색(2)
-public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static int[] arr, used;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        arr = new int[M];
-        used = new int[N+1];
-    }
+public class Main {
+    static int N, M;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+    static boolean isUsed;
 
     static void rec_func(int k) {
-        if(k == M) {
-            for(int i=0; i<M; i++) {
-                sb.append(arr[i]).append(' ');
+        if(k == M+1) {
+            for(int i=1; i<=M; i++) {
+                sb.append(selected[i]).append(" ");
             }
-            sb.append('\n');
-        }
-        else {
-            for(int cand=1; cand<=N; cand++) {
-                if(used[cand] == 1) {
-                    continue;
+            sb.append("\n");
+        } else {
+            for(int i=1; i<=N; i++) {
+                isUsed = false;
+                for(int j=1; j<k; j++) {
+                    if(i == selected[j]) {
+                        isUsed = true;
+                    }
                 }
-                arr[k] = cand;
-                used[cand] = 1;
-                rec_func(k+1);
-                used[cand] = 0;
+                if(!isUsed) {
+                    selected[k] = i;
+                    rec_func(k+1);
+                    selected[k] = 0;
+                }
             }
         }
     }
 
-    public static void main(String[] args) {
-        input();
-        rec_func(0);
-        System.out.println(sb.toString());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M+1];
+
+        rec_func(1);
+        System.out.println(sb);
     }
 }
